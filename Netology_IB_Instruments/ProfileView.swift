2d445d2d636225b00e3983d439eab9d2d3883e70
Evgenii Mikhailov <e.mikhailov@outlook.com>
2022-11-18 22:53:30 +0500

    textView fix

--- Netology_IB_Instruments/ProfileView.swift
+++ Netology_IB_Instruments/ProfileView.swift
@@ -31,6 +31,7 @@ class ProfileView: UIView {
         locationCityTextLabel.layer.masksToBounds = true
         locationCityTextLabel.layer.cornerRadius = 10
         self.profileDetailsTextView.text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+        profileDetailsTextView.isEditable = false
     }
     
     
